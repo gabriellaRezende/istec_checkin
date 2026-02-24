9fcbbd5379c7f789da11fd0a64856005a77e1298
Gabriella <gabriella.rubinrez@hotmail.com>
2026-02-24 14:10:20 +0000

    Atualiza comentários no código para melhorar a clareza e entendimento das funcionalidades da classe AppState

--- lib/providers/app_state.dart
+++ lib/providers/app_state.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/check_in.dart';
 
+// AppState é a classe que gerencia o estado global da aplicação, incluindo o status de autenticação do usuário e o histórico de check-ins. Ela utiliza ChangeNotifier para notificar os widgets que dependem desses dados quando houver mudanças, permitindo uma atualização reativa da interface do usuário.
 class AppState with ChangeNotifier {
   bool _isLoggedIn = false;
   List<CheckInRecord> _history = [];
@@ -12,41 +13,48 @@ class AppState with ChangeNotifier {
   List<CheckInRecord> get history => _history;
 
   AppState() {
-    _loadHistory();
+    _loadHistory(); // Carrega o histórico de check-ins do armazenamento local quando a aplicação é iniciada.
   }
 
+  //Gerencia o processo de login. Apenas simula a autenticação e verifica se o campo de ID e senha estão vazios. 
+  // Se estiver preenchido >> Login bem sucedido
+  // Se estiver vazio >> Login falhou
   void login(String id, String pass) {
     if (id.isNotEmpty && pass.isNotEmpty) {
       _isLoggedIn = true;
-      notifyListeners();
+      notifyListeners(); //Aqui vai notificar os widgets que dependem da resposta do login para atualizar a interface.
     }
   }
 
+  //Gerencia o logout. Muda o estado de login para false e notifica os widgets para alterar a interface de volta para o login screen.
   void logout() {
     _isLoggedIn = false;
     notifyListeners();
   }
 
+  //Caso o novo registro de check-in for bem sucedido, ou seja, ele for válido, então adiciona na lista de hitórico e salva localmente.
   void addRecord(CheckInRecord record) {
     _history.insert(0, record);
-    _saveHistory();
-    notifyListeners();
+    _saveHistory(); // Salva o histórico atualizado no armazenamento local.
+    notifyListeners(); //Notifica os widgets para alterar o hitórico.
   }
 
+  //Carrega o histórico do armazenamento local. 
   Future<void> _loadHistory() async {
     try {
       final prefs = await SharedPreferences.getInstance();
       final String? historyData = prefs.getString('history');
       if (historyData != null && historyData.isNotEmpty) {
         final List decoded = jsonDecode(historyData);
-        _history = decoded.map((item) => CheckInRecord.fromJson(item)).toList();
-        notifyListeners();
+        _history = decoded.map((item) => CheckInRecord.fromJson(item)).toList(); //Converte os dados locais para a lista de registos
+        notifyListeners(); //Notifica o widget responsável para a tualizar a interface do histórico exatamente com os dados que estão salvos localmente.
       }
     } catch (e) {
       debugPrint('Erro ao carregar histórico: $e');
-    }
+    } //catch para tratar erro caso exista algum problema ao acessar o histórico.
   }
 
+  // Salva o histórico atualizado localmente. 
   Future<void> _saveHistory() async {
     try {
       final prefs = await SharedPreferences.getInstance();
