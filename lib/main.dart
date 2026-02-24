fcfd71ee1f393821b6da436df17deada7dbc97fd
Gabriella <gabriella.rubinrez@hotmail.com>
2026-02-24 13:54:33 +0000

    Atualiza comentários no código para melhor clareza e entendimento da estrutura do aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -6,24 +6,27 @@ import 'package:istec_checkin/screens/dashboard_screen.dart';
 import 'package:istec_checkin/screens/history_screen.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // inicializa o flutter e o sistema nativo.
   runApp(
+    // Inicia com o multiprovider que injeta o AppState para a aplicação.
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => AppState()), // Gerenciamento de estado global. Pode ser expandido para incluir autenticação, dados do usuário, etc.
+        ChangeNotifierProvider(create: (_) => AppState()), // Toda a aplicação terá acesso ao AppState, que gerencia o estado global.
       ],
       child: const IstecApp(),
     ),
   );
 }
 
-// Widget raiz do aplicativo. Configura o tema, rotas e a tela inicial. Pode ser expandido para incluir suporte a múltiplos idiomas, temas dinâmicos, etc.
+
 class IstecApp extends StatelessWidget {
   const IstecApp({super.key});
 
+// Metodo build retorna um widget.
+// Aqui retorna o MaterialApp que defini o tema e a estrutura do aplicativo.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp( //Define o tema, titulo e a tela home.
       title: 'ISTEC Check-in',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -34,7 +37,7 @@ class IstecApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: const AuthWrapper(),
+      home: const AuthWrapper(), //retorna a classe AuthWrapper que decide qual tela mostrar com base no estado de autenticação do usuário.
     );
   }
 }
@@ -50,7 +53,7 @@ class AuthWrapper extends StatelessWidget {
   }
 }
 
-// Widget de navegação principal que gerencia as telas de dashboard e histórico. Pode ser expandido para incluir mais telas, animações de transição, etc. 
+// Widget de navegação principal que gerencia as telas de dashboard e histórico. 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
 
@@ -59,13 +62,14 @@ class MainNavigation extends StatefulWidget {
 }
 
 class _MainNavigationState extends State<MainNavigation> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 0; // Índice da tela atualmente selecionada na barra de navegação. Ou seja, inicia com a tela de dashboard selecionada. Altera para 1 quando clica no histórico.
 
   static const List<Widget> _screens = [
     DashboardScreen(), //Tela principal do app 
     HistoryScreen(), //Tela de histórico de check-ins do usuário
   ];
 
+//Scafoold retorna com a estrutura da tela que foi selecionada na navegação.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
